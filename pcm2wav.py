efd4aacc21aa2b5c814faf4f50425815494474d8
Kris Jeong <kjeong@stanford.edu>
2021-08-06 13:04:34 +0900

    Fixed bug due to os.chdir

--- pcm2wav.py
+++ pcm2wav.py
@@ -1,22 +1,19 @@
 import subprocess
 import os
+#!/usr/bin/env python3
 
-root = "~"
-# directory = "datasets_korean/train_data"
-directory = "datasets_korean_copy/train_data"
+directory = os.path.abspath("datasets_korean/train_data")
+# directory = os.path.abspath("datasets_korean_copy/train_data")
 
 def main():
     for dir1 in os.listdir(directory):  # KsponSpeech_01
         dir1_filepath = os.path.join(directory, dir1)
         for dir2 in os.listdir(dir1_filepath):    # KsponSpeech_0001
-            dir2_filepath = os.path.join(dir1_filepath, dir2)       # dir2_filepath = full filepath we need to move to
-            os.chdir(f"{dir2_filepath}")        # FileNotFoundError: [Errno 2] No such file or directory: 'datasets_korean_copy/train_data'
+            dir2_filepath = os.path.join(dir1_filepath, dir2)
+            os.chdir(dir2_filepath)
             print(os.getcwd())
-            # subprocess.run(['sh', "/home/kris/workspace/repo/fairseq/pcm2wav.sh"])
-            os.chdir("..")    # TODO: need to get back to original dir
-            print(os.getcwd())
-        os.chdir("..")  # TODO: need to get back to original dir
-        print(os.getcwd())
+            subprocess.run(['sh', "/home/kris/workspace/repo/fairseq/pcm2wav.sh"])
+
 if __name__ == '__main__':
     main()
 
